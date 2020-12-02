f4b779bf91942ac89e2e951dec67340b58e54d69
Khayal Teyyubov <teyyubovxeyal04@gmail.com>
2020-12-02 18:36:30 +0400

    Seeding part for Products and Reviews

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -22,7 +22,12 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word,
+            'detail' => $this->faker->paragraph,
+            'price' => $this->faker->numberBetween(100, 1000),
+            'stock' => $this->faker->randomDigit,
+            'discount' => $this->faker->numberBetween(2, 30),
+
         ];
     }
 }
