ce299bfa74208dd4ed425a001a74a46e61e93da3
Khayal Teyyubov <teyyubovxeyal04@gmail.com>
2020-12-03 14:24:31 +0400

    Product Collection

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,16 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'totalprice' => round((1 - $this->discount / 100) * $this->price, 2),
+            'rating' => $this->review->count() > 0 ? round($this->review->sum('star') / $this->review->count(), 2)
+                : 'No Rating Yet!',
+            'discount' => $this->discount,
+            'href' => [
+                'link' => route('products.show', $this->id)
+
+            ],
+        ];
     }
 }
